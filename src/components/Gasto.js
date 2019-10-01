50d2b440c856a34fde1e81cc14993d91170f7c38
DanielCornelio <jdcornelio89@gmail.com>
2019-10-01 12:54:54 -0500

    Proyecto Gasto Semanal

---
+++ src/components/Gasto.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Gasto extends Component{
+    render(){
+        return(
+            <p>desdegasgto</p>
+        )
+    }
+}
+export default Gasto;
